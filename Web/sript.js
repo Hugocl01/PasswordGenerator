2e6a2b0fcfc64a3b5af3afb621b16cc9c7ecb238
Hugocl01 <hugocayon@gmail.com>
2023-06-22 19:10:59 +0200

    Cambios en la pagina

--- Web/sript.js
+++ Web/sript.js
@@ -1,4 +1,16 @@
 //Funcion para cambiar de modo (dia o noche)
+const temaOscuro = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "dark");
+    document.querySelector("dl-icon").setAttribute("class", "bi bi-sun-fill");
+}
+const temaClaro = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "light");
+    document.querySelector("dl-icon").setAttribute("class", "bi bi-moon-fill");
+}
+const cambiarTema = () => {
+    document.querySelector("body").setAttribute("data-bs-theme") === "light" ?
+        temaOscuro() : temaClaro();
+}
 /*
 const toggle = document.getElementsById('toggleDark');
 const body = document.querySelector('body');
@@ -16,7 +28,7 @@ toggle.addEventListener('click', function() {
     }
 })
 */
-
+/*
 const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
 function cambiaTema(ev) {
     if (ev.target.checked) {
@@ -26,3 +38,4 @@ function cambiaTema(ev) {
     }
 }
 colorSwitch.addEventListener('change', cambiaTema);
+*/
