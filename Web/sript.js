0d3f424b42049d2418cc8a197492e279c4f4e12a
Hugocl01 <hugocayon@gmail.com>
2023-06-22 04:28:33 +0200

    cambios en la pagina web

---
+++ Web/sript.js
@@ -0,0 +1,28 @@
+//Funcion para cambiar de modo (dia o noche)
+/*
+const toggle = document.getElementsById('toggleDark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function() {
+    this.classList.toggle('bi-moon')
+    if (this.classList.toggle('bi-brightness-high-fill')) {
+        body.style.background = 'white';
+        body.style.color = 'black';
+        body.style.transition = '2s';
+    } else {
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '2s';
+    }
+})
+*/
+
+const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
+function cambiaTema(ev) {
+    if (ev.target.checked) {
+        document.documentElement.setAttribute('tema', 'dark');
+    } else {
+        document.documentElement.setAttribute('tema', 'light');
+    }
+}
+colorSwitch.addEventListener('change', cambiaTema);
